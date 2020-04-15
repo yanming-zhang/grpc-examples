600bf5bd7fd0c81e7524ea190d4628e5da2df07c
rocketzhang <rocketzhang@MacBook-Pro12.local>
2020-04-15 17:34:15 +0800

    update

---
+++ server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/yanming-zhang/grpc-examples/protos"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// 创建一个 gRPC 服务端实例
+	s := grpc.NewServer()
+
+	// 注册
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
